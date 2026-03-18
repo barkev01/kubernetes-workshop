622e752ddad3367bab8205ba38ce86beb1e8e039
mahsereb <barkev.mahseredjian@grenoble-inp.fr>
2026-03-18 22:25:01 +0100

    Updated to Java 21 and ubi9

--- kubernetes/second-service/src/main/java/me/escoffier/workshop/MySecondResource.java
+++ kubernetes/second-service/src/main/java/me/escoffier/workshop/MySecondResource.java
@@ -1,14 +1,14 @@
 package me.escoffier.workshop;
 
 import io.vertx.core.Vertx;
+import jakarta.annotation.PostConstruct;
 import org.jboss.logging.Logger;
 
-import javax.annotation.PostConstruct;
-import javax.inject.Inject;
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
-import javax.ws.rs.core.MediaType;
+import jakarta.inject.Inject;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
 
 import java.io.IOException;
 import java.util.Arrays;
